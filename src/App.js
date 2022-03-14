de3814ad0e49b3cfb69c0113cf676891a2add98c
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-14 08:55:55 +0600

    completed functionality of drag and drop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import "./App.css";
+import DropFileInput from "./components/drop-file-input/DropFileInput";
 
-const App = () => {
+function App() {
+  const onFileChange = (files) => {
+    console.log(files);
+  };
+
   return (
-    <div>App</div>
-  )
+    <div className="box">
+      <h2 className="header">React drop files input</h2>
+      <DropFileInput onFileChange={(files) => onFileChange(files)} />
+    </div>
+  );
 }
 
-export default App
+export default App;
